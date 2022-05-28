82f2f72f37dc2a94d9e59749a38e03f6a6c72968
DesignofCp <4IN1Telecommunications@gmail.com>
2022-05-27 20:53:24 -0500

    Update

---
+++ BusinessCellular/BusinessCellular/BusinessCellularApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCellularApp.swift
+//  BusinessCellular
+//
+//  Created by Charlithia on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCellularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusinessCellular/BusinessCellular/DetailView.swift
@@ -0,0 +1,79 @@
+//
+//  DetailView.swift
+//  BusinessCellular
+//
+//  Created by Charlithia on 5/27/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let contact: Contact
+    
+    var body: some View {
+        VStack {
+            Image(contact.imageName)
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipped()
+                .cornerRadius(150)
+                .shadow(radius: 3)
+            Text(contact.name)
+                .font(.title)
+                .fontWeight(.medium)
+            
+            Form {
+                Section {
+                    
+                    HStack{
+                            Text("Phone")
+                            Spacer()
+                            Text(contact.phone)
+                                .foregroundColor(.white)
+                                .font(.callout)
+                        }
+                        
+                    HStack{
+                            Text("Email")
+                            Spacer()
+                            Text(contact.email)
+                                .foregroundColor(.white)
+                                .font(.callout)
+                        }
+                        
+                        HStack{
+                            Text("Adreess")
+                            Spacer()
+                            Text(contact.address)
+                                .foregroundColor(.white)
+                                .font(.callout)
+                                .frame(width: 180)
+                    }
+                }
+                Section {
+                    Button(action:  {
+                        print("Send a Message")
+                        
+                    }){
+                        Text("Send a Message")
+                    }
+                    Button(action: {
+                        print("Call")
+                    }){
+                        Text("Call")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(contact: contacts[0])
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13 Pro Max")
+            .padding(.top, 60)
+    }
+}
